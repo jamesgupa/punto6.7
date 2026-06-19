cfca4c05d8010665158226978bbe8e173eb1795c
jamesgupa <93831613+jamesgupa@users.noreply.github.com>
2026-06-18 20:59:26 -0500

    Add files via upload

---
+++ com/mycompany/ejercicio_418/Ejercicio_418.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_418;
+
+/**
+ *
+ * @author JGP
+ */
+public class Ejercicio_418 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
